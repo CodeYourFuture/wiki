f6ecbabb3a989804e821469fd0d1f246af7f1a48
Chris Owen <cowen1992@hotmail.co.uk>
2020-01-29 17:07:24 +0000

    Change birmingham to rome

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -57,7 +57,7 @@ class HomeSplash extends React.Component {
             <Button href={docUrl("cities/glasgow/home")}>Glasgow</Button>
             <Button href={docUrl("cities/manchester/home")}>Manchester</Button>
             <Button href={docUrl("cities/birmingham/home")}>Birmingham</Button>
-            <Button href={docUrl("cities/rome/home")}>Birmingham</Button>
+            <Button href={docUrl("cities/rome/home")}>Rome</Button>
           </PromoSection>
         </div>
       </SplashContainer>
